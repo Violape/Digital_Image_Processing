913611048d9f05708f441feb0464e53ef8e6f8d3
Violape <Dr_Otaku@yeah.net>
2019-05-08 02:16:14 +0800

    Part 1 - Negative Film

--- __init__.py
+++ __init__.py
@@ -3,9 +3,15 @@ from video_demo import video_demo
 from split import split
 from logic import logic
 from equal_hist import equal_hist
-
+from image_enhancing import image_enhancement
+# Testing OpenCV
+'''
 show_image()
 video_demo()
 split()
 logic()
 equal_hist()
+'''
+
+# Experiment: Image Enhancement
+image_enhancement()
---
+++ image_enhancing.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+
+def image_enhancement():
+    exp1()
+
+
+def exp1():  # 负像变换
+    src = cv2.imread('res/moon.tif')
+    img_info = src.shape
+    image_height = img_info[0]
+    image_weight = img_info[1]
+    dst = np.zeros((image_height, image_weight, 3), np.uint8)
+    for i in range(image_height):
+        for j in range(image_weight):
+            (b, g, r) = src[i][j]
+            dst[i][j] = (255 - b, 255 - g, 255 - r)
+    cv2.imshow('Original Film', src)
+    cv2.imshow('Negative Film', dst)
+    cv2.waitKey(0)
